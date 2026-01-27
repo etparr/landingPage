cded29047cacee9e80592344a0ae683f068c6611
etparr <ethanparr8@gmail.com>
2026-01-26 20:29:46 -0500

    Add about page and Vite build entry

--- gradebot-static/vite.config.js
+++ gradebot-static/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         pricing: resolve(__dirname, 'pricing.html'),
         'getting-started': resolve(__dirname, 'getting-started.html'),
+        about: resolve(__dirname, 'about.html'),
       },
     },
   },
